6c916e00a87ff066ded73ab1ae61a99a2a9f690e
Lloyd Briggs <lloydb1@gmail.com>
2018-07-06 23:16:48 -0700

    resolving cleanup issues

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,5 @@
-import { createBrowserHistory } from 'history';
-import { routerMiddleware } from 'react-router-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { responsiveStoreEnhancer } from 'redux-responsive';
-import ROUTES from './common/constants/routes';
 import thunk from 'redux-thunk';
 import rootReducer from './common/reducers';
 
@@ -17,7 +14,6 @@ export const composeFunc = () => {
 
 export const enhancers = [
   thunk,
-  routerMiddleware(createBrowserHistory({ basename: ROUTES.BASE_ROUTE })),
 ].map(a => applyMiddleware(a));
 enhancers.push(responsiveStoreEnhancer);
 
