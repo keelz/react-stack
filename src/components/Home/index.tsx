ec65988cdd40292bc70b1c3396a1685f4e64d888
Lloyd Briggs <lloydb1@gmail.com>
2018-07-06 17:31:38 -0700

    style

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 
-const Home: React.SFC = () => <div className="Home">home</div>;
+const Home: React.SFC = () => <div className="Home">Home</div>;
 
 export default Home;
